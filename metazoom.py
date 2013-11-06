d115dde17caaf712d3c3e3196606285b512d7120
Nicolas Loira <nloira@gmail.com>
2013-11-06 13:14:08 -0300

    NL: getting element ids to the screen

--- metazoom.py
+++ metazoom.py
@@ -12,6 +12,7 @@ import sys,os
 import getopt
 import curses
 import time
+import random
 
 import basicSBML
 
@@ -73,15 +74,13 @@ def mainTUI(*args, **kwds):
 	# setup curses
 	curses.init_pair(1, curses.COLOR_RED, curses.COLOR_WHITE)
 	curses.curs_set(0) # invisible cursor
-
-	(my,mx) = mainw.getmaxyx()
 	mainw.nodelay(0) # wait for user input
 
-	# some feedback
-	mainw.addstr(5,2, str(len(model.reactions)), curses.color_pair(1))
-	
-	# Main loop
+	# start metazoom layout engine
+	mz = MZlayout(mainw, model)
+	mz.printAtCenter("Welcome to MetaZoom")
 
+	# Main loop
 	while True:
 		# get command
 		key=mainw.getch()
@@ -91,11 +90,69 @@ def mainTUI(*args, **kwds):
 		mainw.erase()
 
 		# execute command
-		mainw.addstr(5,2, keyname)
+		mz.command(keyname)
 
 		# update screen
+		mz.redraw()
 		mainw.noutrefresh()
 		curses.doupdate()
+
+###############################################################
+
+
+class MZlayout():
+	REACTION = 1
+	SPECIES = 2
+
+	def __init__(self, mainw, model):
+		self.model=model
+		self.mainw=mainw
+		(self.my,self.mx) = mainw.getmaxyx()
+
+		self.centerOn = None
+		self.cmode = None
+		self.focusOn = None
+		self.distance = 1
+		self.statusBar = ""
+
+	def command(self, keyname):
+		self.centerOnAnyReaction()
+
+	def centerOnAnyReaction(self):
+		assert len(self.model.reactions)>0
+
+		self.centerOn = random.choice(self.model.reactions)
+		self.cmode = MZlayout.REACTION
+
+	def centerOnAnySpecies(self):
+		assert len(self.model.species)>0
+
+		self.centerOn = random.choice(self.model.species)
+		self.cmode = MZlayout.SPECIES
+
+	def printAtCenter(self, label):
+		llabel=len(label)
+		assert llabel>0
+
+		# display label on center of screen
+		y = self.my/2
+		x = (self.mx-llabel)/2
+
+		self.mainw.addstr(y,x,label)
+
+	def redraw(self):
+
+		if self.cmode==MZlayout.REACTION:
+			label = "["+self.centerOn.id+"]"
+		elif self.cmode==MZlayout.SPECIES:
+			label = "("+self.centerOn.id+")"
+		else:
+			assert False, "WRONG CENTER MODEL"
+
+		# display label on center of screen
+		self.printAtCenter(label)
+
+
 
 
 ###############################################################
